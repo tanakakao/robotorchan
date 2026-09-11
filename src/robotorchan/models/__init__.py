3d45f3bd0a3159d7831c477792dce5cb04bcc7ed
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-11 20:24:23 +0900

    feat: export multitask GP wrappers

--- src/robotorchan/models/__init__.py
+++ src/robotorchan/models/__init__.py
@@ -3,10 +3,13 @@
 from robotorchan.models.base import UnsupportedModelOperationError
 from robotorchan.models.mixed import MixedSingleTaskGP
 from robotorchan.models.multi_fidelity import SingleTaskMultiFidelityGP
+from robotorchan.models.multitask import KroneckerMultiTaskGP, MultiTaskGP
 from robotorchan.models.single_task import SingleTaskGP
 
 __all__ = [
+    "KroneckerMultiTaskGP",
     "MixedSingleTaskGP",
+    "MultiTaskGP",
     "SingleTaskGP",
     "SingleTaskMultiFidelityGP",
     "UnsupportedModelOperationError",
