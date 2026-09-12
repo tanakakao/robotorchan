4a05f1e02af1da459a08804e6005661a9f72520f
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-12 15:40:19 +0900

    Fix ReducedGP test formatting

--- tests/models/test_reduced_gp.py
+++ tests/models/test_reduced_gp.py
@@ -14,11 +14,7 @@ from robotorchan.models.reduction import (
 def _training_data() -> tuple[torch.Tensor, torch.Tensor]:
     torch.manual_seed(7)
     train_X = torch.randn(18, 6, dtype=torch.double)
-    train_Y = (
-        1.5 * train_X[:, :1]
-        - 0.7 * train_X[:, 1:2]
-        + 0.2 * train_X[:, 2:3].square()
-    )
+    train_Y = 1.5 * train_X[:, :1] - 0.7 * train_X[:, 1:2] + 0.2 * train_X[:, 2:3].square()
     return train_X, train_Y
 
 
