b7ce8c0e0d177dc3c998636738c7d6facf70b1ee
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-12 15:19:50 +0900

    Export reduced GP model wrappers

--- src/robotorchan/models/__init__.py
+++ src/robotorchan/models/__init__.py
@@ -22,6 +22,7 @@ from robotorchan.models.model_list import ModelListGP
 from robotorchan.models.multi_fidelity import SingleTaskMultiFidelityGP
 from robotorchan.models.multitask import KroneckerMultiTaskGP, MultiTaskGP
 from robotorchan.models.pairwise import PairwiseGP
+from robotorchan.models.reduced import PCAGP, PLSGP, RandomProjectionGP, ReducedGP
 from robotorchan.models.robust import RobustRelevancePursuitSingleTaskGP
 from robotorchan.models.single_task import MixedSingleTaskGP, SingleTaskGP
 from robotorchan.models.variational import SingleTaskVariationalGP
@@ -42,7 +43,11 @@ __all__ = [
     "ModelListGP",
     "MultiTaskGP",
     "OrthogonalAdditiveGP",
+    "PCAGP",
+    "PLSGP",
     "PairwiseGP",
+    "RandomProjectionGP",
+    "ReducedGP",
     "RobustRelevancePursuitSingleTaskGP",
     "SaasFullyBayesianMultiTaskGP",
     "SaasFullyBayesianSingleTaskGP",
