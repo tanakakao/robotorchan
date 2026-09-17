567161d152b4684d1acb006053beb9585a55329a
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-17 10:08:04 +0900

    Expose latent reconstruction capabilities

--- src/robotorchan/optim/__init__.py
+++ src/robotorchan/optim/__init__.py
@@ -1,11 +1,19 @@
 """Acquisition-function optimization and search-strategy interfaces."""
 
 from robotorchan.optim.base import SearchResult, SearchStrategy
+from robotorchan.optim.latent import (
+    LatentReconstruction,
+    PCAReconstruction,
+    RandomProjectionReconstruction,
+)
 from robotorchan.optim.original import OriginalSpaceStrategy
 from robotorchan.optim.random import RandomSearchStrategy
 
 __all__ = [
+    "LatentReconstruction",
     "OriginalSpaceStrategy",
+    "PCAReconstruction",
+    "RandomProjectionReconstruction",
     "RandomSearchStrategy",
     "SearchResult",
     "SearchStrategy",
