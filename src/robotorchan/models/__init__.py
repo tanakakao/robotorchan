d968decc8c28443ca5f36299e0ba055efc4d826a
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-18 22:15:03 +0900

    Organize reduced model imports

--- src/robotorchan/models/__init__.py
+++ src/robotorchan/models/__init__.py
@@ -32,14 +32,14 @@ from robotorchan.models.reduced.base import (
     RandomProjectionGP,
     ReducedGP,
 )
+from robotorchan.models.reduced.joint_neural import HybridAutoEncoderGP, JointEncoderGP
+from robotorchan.models.reduced.joint_vae import JointVAEGP
 from robotorchan.models.reduced.mixed import (
     MixedPCAGP,
     MixedPLSGP,
     MixedRandomProjectionGP,
     MixedReducedGP,
 )
-from robotorchan.models.reduced.joint_neural import HybridAutoEncoderGP, JointEncoderGP
-from robotorchan.models.reduced.joint_vae import JointVAEGP
 from robotorchan.models.reduced.supervised_neural import SupervisedAutoEncoderGP
 from robotorchan.models.reduced.vae import VAEGP, SupervisedVAEGP
 from robotorchan.models.robust import RobustRelevancePursuitSingleTaskGP
