2ec2b94a08c98a456489277c313e94542f8803fb
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-19 11:22:34 +0900

    Fix reduced multitask format gate

--- src/robotorchan/models/reduced/multitask.py
+++ src/robotorchan/models/reduced/multitask.py
@@ -103,9 +103,7 @@ class ReducedMultiTaskGP(MultiTaskGP):
         return super().posterior(self._prepare_inputs(X), *args, **kwargs)
 
     def condition_on_observations(self, X: Tensor, Y: Tensor, **kwargs: Any):
-        return super().condition_on_observations(
-            X=self._prepare_inputs(X), Y=Y, **kwargs
-        )
+        return super().condition_on_observations(X=self._prepare_inputs(X), Y=Y, **kwargs)
 
 
 class ReducedKroneckerMultiTaskGP(KroneckerMultiTaskGP):
