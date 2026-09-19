5416e83e7d10114efad621c5ddd1909dc6c767c8
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-19 21:41:28 +0900

    test: register replicate-noise public contract

--- tests/models/test_public_model_contracts.py
+++ tests/models/test_public_model_contracts.py
@@ -77,6 +77,7 @@ PUBLIC_MODEL_NAMES = {
     "ReducedGP",
     "ReducedKroneckerMultiTaskGP",
     "ReducedMultiTaskGP",
+    "ReplicateNoiseSingleTaskGP",
     "RobustRelevancePursuitSingleTaskGP",
     "SACGP",
     "SaasFullyBayesianMultiTaskGP",
