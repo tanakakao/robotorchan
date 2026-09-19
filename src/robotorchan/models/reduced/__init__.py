105ee81bf9c0960b716f9ba7ccf7ce4d7fabf5e9
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-19 13:58:09 +0900

    Export mixed reduced multitask models

--- src/robotorchan/models/reduced/__init__.py
+++ src/robotorchan/models/reduced/__init__.py
@@ -29,6 +29,10 @@ from robotorchan.models.reduced.joint_neural import (
 )
 from robotorchan.models.reduced.joint_vae import JointVAEGP, MixedJointVAEGP
 from robotorchan.models.reduced.mixed import MixedReducedGP
+from robotorchan.models.reduced.mixed_multitask import (
+    MixedReducedKroneckerMultiTaskGP,
+    MixedReducedMultiTaskGP,
+)
 from robotorchan.models.reduced.multitask import (
     AutoEncoderKroneckerMultiTaskGP,
     AutoEncoderMultiTaskGP,
@@ -82,6 +86,8 @@ __all__ = [
     "MixedPLSGP",
     "MixedRandomProjectionGP",
     "MixedReducedGP",
+    "MixedReducedKroneckerMultiTaskGP",
+    "MixedReducedMultiTaskGP",
     "MixedSupervisedAutoEncoderGP",
     "MixedSupervisedVAEGP",
     "MixedVAEGP",
