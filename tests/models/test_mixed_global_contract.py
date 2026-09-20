3f260e1b0c35df8e769f59d26ec120fa3d637d3e
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-20 13:10:54 +0900

    test: register nonstationary multitask models

--- tests/models/test_mixed_global_contract.py
+++ tests/models/test_mixed_global_contract.py
@@ -22,6 +22,7 @@ EXPECTED_MIXED_MODELS = {
     "MixedLCEMGP",
     "MixedLatentKroneckerGP",
     "MixedMultiTaskGP",
+    "MixedNonstationaryMultiTaskGP",
     "MixedNonstationarySingleTaskGP",
     "MixedOrthogonalAdditiveGP",
     "MixedPCAGP",
