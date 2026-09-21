2f61f37b2c8fbe2530a7786604956d26af0b99d2
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-21 10:31:58 +0900

    Fix stale model imports in mixed_multitask.py

--- src/robotorchan/models/high_dimensional/reduced/mixed_multitask.py
+++ src/robotorchan/models/high_dimensional/reduced/mixed_multitask.py
@@ -15,7 +15,7 @@ from gpytorch.priors import Prior
 from torch import Tensor
 
 from robotorchan.models.base import normalize_feature_dims
-from robotorchan.models.multitask import MixedKroneckerMultiTaskGP, MixedMultiTaskGP
+from robotorchan.models.standard.multitask import MixedKroneckerMultiTaskGP, MixedMultiTaskGP
 from robotorchan.models.high_dimensional.reduced.mixed import MixedInputReducer
 from robotorchan.reduction.base import InputReducer
 
