62820caf070f07ee83a3982fce32c32abd1d0add
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-21 19:27:50 +0900

    feat: expose regression active learning acquisitions

---
+++ src/robotorchan/acquisition/active_learning/__init__.py
@@ -0,0 +1,5 @@
+"""Regression active-learning acquisition functions."""
+
+from robotorchan.acquisition.active_learning.variance import PosteriorStd, PosteriorVariance
+
+__all__ = ["PosteriorStd", "PosteriorVariance"]
