5d431d362c2273cee81d1628d70ce3d290427ecf
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-21 15:36:59 +0900

    Export non-GP acquisition helpers

--- src/robotorchan/acquisition/__init__.py
+++ src/robotorchan/acquisition/__init__.py
@@ -1,1 +1,5 @@
 """Acquisition-function extensions for BoTorch."""
+
+from robotorchan.acquisition.non_gp import make_non_gp_acquisition, validate_non_gp_acquisition
+
+__all__ = ["make_non_gp_acquisition", "validate_non_gp_acquisition"]
