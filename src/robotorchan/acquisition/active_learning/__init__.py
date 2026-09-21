65b7161131acad7fcd6ffac88fa6423e4e08700f
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-21 21:30:19 +0900

    feat: export EPIG acquisition

--- src/robotorchan/acquisition/active_learning/__init__.py
+++ src/robotorchan/acquisition/active_learning/__init__.py
@@ -1,11 +1,13 @@
 """Regression active-learning acquisition functions."""
 
+from robotorchan.acquisition.active_learning.epig import ExpectedPredictiveInformationGain
 from robotorchan.acquisition.active_learning.randomized_straddle import RandomizedStraddle
 from robotorchan.acquisition.active_learning.straddle import BoundaryVariance, Straddle
 from robotorchan.acquisition.active_learning.variance import PosteriorStd, PosteriorVariance
 
 __all__ = [
     "BoundaryVariance",
+    "ExpectedPredictiveInformationGain",
     "PosteriorStd",
     "PosteriorVariance",
     "RandomizedStraddle",
