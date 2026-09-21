5a73d16d65e572371cef35690f04924e03d05a18
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-21 15:03:51 +0900

    Export Random Forest from non-GP family

--- src/robotorchan/models/non_gp/__init__.py
+++ src/robotorchan/models/non_gp/__init__.py
@@ -1,5 +1,6 @@
-"""Infrastructure for non-GP probabilistic surrogate models."""
+"""Non-GP probabilistic surrogate models."""
 
 from robotorchan.models.non_gp.posterior import make_ensemble_posterior
+from robotorchan.models.non_gp.random_forest import RandomForestSurrogate
 
-__all__ = ["make_ensemble_posterior"]
+__all__ = ["RandomForestSurrogate", "make_ensemble_posterior"]
