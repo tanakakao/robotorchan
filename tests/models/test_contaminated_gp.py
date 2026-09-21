294e4e67ebe0903d849524aa8f8de530323d147a
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-21 10:43:57 +0900

    Update model import path in tests/models/test_contaminated_gp.py

--- tests/models/test_contaminated_gp.py
+++ tests/models/test_contaminated_gp.py
@@ -3,7 +3,7 @@
 import torch
 from botorch.acquisition.monte_carlo import qUpperConfidenceBound
 
-from robotorchan.models.contaminated import ContaminatedSingleTaskGP
+from robotorchan.models.robust_models.contaminated import ContaminatedSingleTaskGP
 
 
 def _data() -> tuple[torch.Tensor, torch.Tensor]:
