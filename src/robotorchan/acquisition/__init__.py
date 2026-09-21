1773e74b8e6ef6cc791330a9b47040173a17ac09
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-21 19:27:55 +0900

    feat: export regression active learning acquisitions

--- src/robotorchan/acquisition/__init__.py
+++ src/robotorchan/acquisition/__init__.py
@@ -1,10 +1,13 @@
 """Acquisition-function extensions for BoTorch."""
 
+from robotorchan.acquisition.active_learning import PosteriorStd, PosteriorVariance
 from robotorchan.acquisition.non_gp import make_non_gp_acquisition, validate_non_gp_acquisition
 from robotorchan.acquisition.sampling import select_thompson_candidates
 
 __all__ = [
     "make_non_gp_acquisition",
+    "PosteriorStd",
+    "PosteriorVariance",
     "select_thompson_candidates",
     "validate_non_gp_acquisition",
 ]
