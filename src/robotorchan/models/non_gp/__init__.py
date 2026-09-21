331fc430263a842fb3ac91cc634b1b6f29ac74d5
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-21 16:42:31 +0900

    Export gradient boosting surrogate

--- src/robotorchan/models/non_gp/__init__.py
+++ src/robotorchan/models/non_gp/__init__.py
@@ -1,7 +1,9 @@
 """Non-GP probabilistic surrogate models."""
 
 from robotorchan.models.non_gp.extra_trees import ExtraTreesSurrogate
+from robotorchan.models.non_gp.gradient_boosting import GradientBoostingSurrogate
 from robotorchan.models.non_gp.posterior import make_ensemble_posterior
 from robotorchan.models.non_gp.random_forest import RandomForestSurrogate
 
-__all__ = ["ExtraTreesSurrogate", "RandomForestSurrogate", "make_ensemble_posterior"]
+__all__ = [
+    "GradientBoostingSurrogate","ExtraTreesSurrogate", "RandomForestSurrogate", "make_ensemble_posterior"]
