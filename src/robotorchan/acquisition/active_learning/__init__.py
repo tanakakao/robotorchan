4d743d3ef77414d81e7d86cbff1e005d09479a2a
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-21 20:08:06 +0900

    feat: export level-set acquisitions

--- src/robotorchan/acquisition/active_learning/__init__.py
+++ src/robotorchan/acquisition/active_learning/__init__.py
@@ -1,5 +1,6 @@
 """Regression active-learning acquisition functions."""
 
+from robotorchan.acquisition.active_learning.straddle import BoundaryVariance, Straddle
 from robotorchan.acquisition.active_learning.variance import PosteriorStd, PosteriorVariance
 
-__all__ = ["PosteriorStd", "PosteriorVariance"]
+__all__ = ["BoundaryVariance", "PosteriorStd", "PosteriorVariance", "Straddle"]
