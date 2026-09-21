af405c72bc160a2f44c2b4a3e87857e1d0f08511
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-21 10:47:54 +0900

    Migrate model import in tests/optim/test_original_space_strategy.py

--- tests/optim/test_original_space_strategy.py
+++ tests/optim/test_original_space_strategy.py
@@ -4,7 +4,7 @@ import pytest
 import torch
 from botorch.acquisition.analytic import PosteriorMean
 
-from robotorchan.models.reduced import PCAGP
+from robotorchan.models.high_dimensional.reduced import PCAGP
 from robotorchan.models.single_task import SingleTaskGP
 from robotorchan.optim import OriginalSpaceStrategy
 
