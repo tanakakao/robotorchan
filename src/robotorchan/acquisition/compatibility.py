ff07736084193b8350d9949152a84340c7ed1dbe
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-22 17:07:48 +0900

    Enforce fantasy-model acquisition compatibility

--- src/robotorchan/acquisition/compatibility.py
+++ src/robotorchan/acquisition/compatibility.py
@@ -38,6 +38,8 @@ def check_model_acquisition_compatibility(
     if model_capabilities.non_gp and not acquisition_capabilities.monte_carlo:
         reason = "non-GP empirical ensembles require BoTorch Monte Carlo acquisitions"
         reasons.append(reason)
+    if acquisition_capabilities.requires_fantasize and not model_capabilities.supports_fantasize:
+        reasons.append("acquisition requires fantasy-model support")
     if model_capabilities.ensemble_posterior and not acquisition_capabilities.supports_ensemble:
         reasons.append("acquisition does not support ensemble posteriors")
     if (
