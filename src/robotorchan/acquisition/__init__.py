ca160bf9be8b62be2dc3fec84128751adb91d0ce
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-22 21:16:48 +0900

    Export capability-aware sampler factory

--- src/robotorchan/acquisition/__init__.py
+++ src/robotorchan/acquisition/__init__.py
@@ -9,6 +9,7 @@ from robotorchan.acquisition.active_learning import (
     Straddle,
 )
 from robotorchan.acquisition.non_gp import make_non_gp_acquisition, validate_non_gp_acquisition
+from robotorchan.acquisition.samplers import make_model_sampler
 from robotorchan.acquisition.sampling import select_thompson_candidates
 
 __all__ = [
@@ -18,6 +19,7 @@ __all__ = [
     "PosteriorVariance",
     "RandomizedStraddle",
     "Straddle",
+    "make_model_sampler",
     "make_non_gp_acquisition",
     "select_thompson_candidates",
     "validate_non_gp_acquisition",
