45c995396900f1b9e161c52a7902bfde09fa441f
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-23 09:33:09 +0900

    feat: publish PCA multi-fidelity model

--- src/robotorchan/models/__init__.py
+++ src/robotorchan/models/__init__.py
@@ -139,6 +139,7 @@ from robotorchan.models.robust.student_t import (
 from robotorchan.models.standard.model_list import ModelListGP
 from robotorchan.models.standard.multi_fidelity import (
     MixedSingleTaskMultiFidelityGP,
+    PCAMultiFidelityGP,
     SingleTaskMultiFidelityGP,
 )
 from robotorchan.models.standard.multitask import (
@@ -174,6 +175,7 @@ __all__ = [
     "LCEAGP",
     "LCEMGP",
     "PCAGP",
+    "PCAMultiFidelityGP",
     "PLSGP",
     "SACGP",
     "VAEGP",
