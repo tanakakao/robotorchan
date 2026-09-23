38298bb7df7f717f0904f501c16b0b47fb03c47e
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-23 11:59:27 +0900

    feat: expose PLS multi-fidelity model

--- src/robotorchan/models/__init__.py
+++ src/robotorchan/models/__init__.py
@@ -142,7 +142,7 @@ from robotorchan.models.robust.student_t import (
 from robotorchan.models.standard.model_list import ModelListGP
 from robotorchan.models.standard.multi_fidelity import (
     MixedSingleTaskMultiFidelityGP,
-    PCAMultiFidelityGP,
+    PCAMultiFidelityGP,\n    PLSMultiFidelityGP,
     SingleTaskMultiFidelityGP,
 )
 from robotorchan.models.standard.multitask import (
@@ -272,7 +272,7 @@ __all__ = [
     "OutputPCAGP",
     "OutputPLSGP",
     "PCAKroneckerMultiTaskGP",
-    "PCAMultiFidelityGP",
+    "PCAMultiFidelityGP",\n    "PLSMultiFidelityGP",
     "PCAMultiTaskGP",
     "PLSKroneckerMultiTaskGP",
     "PLSMultiTaskGP",
