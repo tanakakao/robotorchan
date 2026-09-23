b8f08bffab37e6062543e8b7c912eb5dac0eec5c
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-23 18:49:39 +0900

    feat: export additive MAP-SAAS multi-fidelity GP

--- src/robotorchan/models/standard/__init__.py
+++ src/robotorchan/models/standard/__init__.py
@@ -2,6 +2,7 @@
 
 from robotorchan.models.standard.model_list import ModelListGP
 from robotorchan.models.standard.multi_fidelity import (
+    AdditiveMapSaasMultiFidelityGP,
     MapSaasMultiFidelityGP,
     MixedSingleTaskMultiFidelityGP,
     PCAMultiFidelityGP,
