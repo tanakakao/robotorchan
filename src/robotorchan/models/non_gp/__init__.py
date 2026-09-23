10541a6a1ecda1eb349a3426d6e22f15819e8865
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-23 10:39:20 +0900

    style: sort NGBoost public exports

--- src/robotorchan/models/non_gp/__init__.py
+++ src/robotorchan/models/non_gp/__init__.py
@@ -3,8 +3,8 @@
 from robotorchan.models.non_gp.extra_trees import ExtraTreesSurrogate
 from robotorchan.models.non_gp.gradient_boosting import GradientBoostingSurrogate
 from robotorchan.models.non_gp.hist_gradient_boosting import HistGradientBoostingSurrogate
-from robotorchan.models.non_gp.posterior import make_ensemble_posterior
 from robotorchan.models.non_gp.ngboost import NGBoostSurrogate
+from robotorchan.models.non_gp.posterior import make_ensemble_posterior
 from robotorchan.models.non_gp.random_forest import RandomForestSurrogate
 
 __all__ = [
