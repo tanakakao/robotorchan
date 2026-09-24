1deb101fd961b60d30390a2f49c3d6d7c8a135f1
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-24 17:39:25 +0900

    feat: export protected perturbation builder

--- src/robotorchan/objectives/__init__.py
+++ src/robotorchan/objectives/__init__.py
@@ -1,6 +1,6 @@
 """Risk and quality aggregations for robust Bayesian optimization."""
 
-from robotorchan.objectives.input_perturbation import make_input_perturbation_objective
+from robotorchan.objectives.input_perturbation import (\n    make_input_perturbation_objective,\n    make_protected_perturbation_set,\n)
 from robotorchan.objectives.risk import (
     CVaR,
     Expectation,
@@ -18,6 +18,6 @@ __all__ = [
     "SNRatio",
     "VaR",
     "WorstCase",
-    "make_input_perturbation_objective",
+    "make_input_perturbation_objective",\n    "make_protected_perturbation_set",
     "make_risk_measure",
 ]
